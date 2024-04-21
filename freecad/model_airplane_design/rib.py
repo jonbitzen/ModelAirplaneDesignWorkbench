5f4a80c0fa606c2cc870e35780613b674ba36f52
Jonathan E. Railsback <jonbitzen@hotmail.com>
2024-04-21 09:34:21 -0400

    set up rib to be a proper document object with user-tweakable properties

--- freecad/model_airplane_design/rib.py
+++ freecad/model_airplane_design/rib.py
@@ -1,8 +1,7 @@
 import FreeCAD as App
 from . import airfoil
-from enum import Enum
 from typing import Tuple
-from typing import Optional
+from typing import List
 
 def create(
     obj_name: str,
@@ -32,43 +31,45 @@ class Rib():
             "chord",
             "Rib",
             "Rib length from leading edge to trailing edge"
-        ).chord = 1.0
+        ).chord = 100.0
 
         obj.addProperty(
-            "App::PropertyPythonObject",
-            "airfoil_data",
+            "App::PropertyFloat",
+            "thickness",
             "Rib",
-            "Airfoil definition data"
-        ).airfoil_data = None
+            "Rib thickness"
+        ).thickness = 2.0
 
-        # self._airfoil_data = None
+        obj.addProperty(
+            "App::PropertyEnumeration",
+            "airfoil",
+            "Rib",
+            "Airfoil type"
+        ).airfoil = [e.name for e in airfoil.AirfoilType]
+
         obj.Proxy = self
 
-    # @property
-    # def airfoil_data(self) -> Optional[airfoil.AirfoilData]:
-    #     return self._airfoil_data
-    
-    # @airfoil_data.setter
-    # def airfoil_data(self, value: Optional[airfoil.AirfoilData]) -> None:
-    #     self._airfoil_data = value
-
     def onChanged(self, obj: App.DocumentObject, property: str) -> None:
+        """
+        Called when the FeaturePython object properties change.  Note that this
+        includes any properties of the object, in addition to the App::Property*
+        objects that may be added explictly by the user
+        """  
 
-        match property:
-            case "Label":
-                pass
-            case "Proxy":
-                pass
-            case _:
-                print(property + " = " + str(obj.getPropertyByName(property)))
+        # TODO: why is it that when we change a number like the chord we dont
+        #       need to explicitly call execute, but when we change the airfoil
+        #       we do?     
+        if property == "airfoil":
+            self.execute(obj)
 
     def attach(self, obj: App.DocumentObject) -> None:
         pass
 
     def execute(self, obj: App.DocumentObject) -> None:
-        print("Rib.execute")
-
-
+        self.airfoil_data = airfoil.load(airfoil.AirfoilType.to_filename(obj.getPropertyByName("airfoil")))
+        tmp_placement: App.Placement = obj.Placement
+        obj.Shape = self.airfoil_data.to_shape(obj.getPropertyByName("chord"))
+        obj.Placement = tmp_placement
 
 class RibViewProvider():
     def __init__(self, vobj: App.Gui.ViewProviderDocumentObject) -> None:
@@ -87,5 +88,13 @@ class RibViewProvider():
     def onDelete(self, vobj: App.Gui.ViewProviderDocumentObject, subelements: Tuple[str]) -> bool:
         return True
     
+    def claimChildren(self) -> List[App.DocumentObject]:
+        """
+        Claim children of this ViewObject.  Claimed children will be displayed
+        as children underneath this ViewObject.  Each child object should be an
+        App.DocumentObject
+        """
+        return []
+
     def onChanged(self, vobj: App.Gui.ViewProviderDocumentObject, prop: str) -> None:
         pass
