2fc6cdfb4a505097f145f68c97d11cb0e817afa7
Jonathan E. Railsback <jonbitzen@hotmail.com>
2024-05-04 15:19:32 -0400

    its more modular, it works

--- freecad/model_airplane_design/rib_hole_generators.py
+++ freecad/model_airplane_design/rib_hole_generators.py
@@ -8,7 +8,7 @@ import math
 import numpy
 import Part
 import Sketcher
-from typing import List
+from typing import List, Dict
 
 
 class Interval():
@@ -98,9 +98,11 @@ class HoleBoundRegion():
         if interval is None:
             raise ValueError("Hole bound interval must not be None")
 
-        self.af_inner_counter = af_inner_contour
+        self.af_inner_contour = af_inner_contour
         self.interval = interval        
 
+# TODO: rename to HoleIntervalGenerator, since it is a region for one or more
+#       holes
 class HoleBoundGenerator():
     """
         Generates one or more HoleBoundRegions inside a rib, such that the
@@ -195,14 +197,7 @@ class HoleBoundGenerator():
         for doc_obj in self.show_items:
             App.ActiveDocument.removeObject(doc_obj.Name)
 
-class HoleGenerator(ABC):
-    """
-    Interface for classes that will draw lightening holes inside a wing rib
-    """
-    def generate_sketch(self, interval: HoleBoundRegion) -> Sketcher.Sketch:
-        pass
-
-class RoundedTrapezoidSidePoints():
+class RoundedTrapezoidEndcap():
     """
     Helper class which encapsulates control points for the end-caps of a rounded
     trapezoidal lightening hole.
@@ -229,6 +224,15 @@ class RoundedTrapezoidSidePoints():
         """
         return len(self.pts) == 1
 
+class EndcapSide(Enum):
+    LEFT = 1
+    RIGHT = 2
+
+class ChamferPointLocation(Enum):
+    UPPER = 1
+    LOWER = 2
+    CENTER = 3
+
 class RoundedTrapezoidControlPoints():
     """
     Contains the control points used to generate a single rounded trapezoidal rib
@@ -236,7 +240,7 @@ class RoundedTrapezoidControlPoints():
 
     Parameters
     ----------
-    airfoil_profile: Part.Shape
+    rib_inner_profile: Part.Shape
         the inner profile in the rib, which forms the outermost upper and lower
         bound for the rounded trapezoidal hole
     
@@ -249,7 +253,7 @@ class RoundedTrapezoidControlPoints():
     """
     def __init__(
             self,
-            airfoil_profile: Part.Shape,
+            rib_inner_profile: Part.Shape,
             interval: Interval,
             chamfer_rad: float
         ) -> None:
@@ -260,11 +264,13 @@ class RoundedTrapezoidControlPoints():
         self.upper_spline_pts: List[App.Vector] = []
         self.lower_spline_pts: List[App.Vector] = []
 
-        self.left_side_pts: RoundedTrapezoidSidePoints = \
-            self.__get_side_coords(interval.start, airfoil_profile, chamfer_rad)
-        self.right_side_pts: RoundedTrapezoidSidePoints = \
-            self.__get_side_coords(interval.end, airfoil_profile, chamfer_rad)
+        self.left_chamfer_pts: Dict[ChamferPointLocation, App.Vector] = {}
+        self.right_chamfer_pts: Dict[ChamferPointLocation, App.Vector] = {}
 
+        self.left_endcap: RoundedTrapezoidEndcap = \
+            self.__get_side_coords(interval.start, rib_inner_profile, chamfer_rad)
+        self.right_endcap: RoundedTrapezoidEndcap = \
+            self.__get_side_coords(interval.end, rib_inner_profile, chamfer_rad)
 
         # generate control points for the upper and lower splines, which will
         # hug the airfoil interior profile (airfoil_profile)
@@ -275,16 +281,51 @@ class RoundedTrapezoidControlPoints():
         spl_right_bound = interval.end-chamfer_rad
         num_ctl_pts = 5
         for x_coord in numpy.linspace(spl_left_bound, spl_right_bound, num_ctl_pts):
-            pts = self.__get_intersections(x_coord, airfoil_profile)
+            pts = self.__get_intersections(x_coord, rib_inner_profile)
             self.upper_spline_pts.append(pts[0])
             self.lower_spline_pts.append(pts[1])
+
+        self.left_chamfer_pts = self.__set_chamfer_pts(self.left_endcap, EndcapSide.LEFT)
+        self.right_chamfer_pts = self.__set_chamfer_pts(self.right_endcap, EndcapSide.RIGHT)
+
+    def __set_chamfer_pts(
+            self,
+            endcap: RoundedTrapezoidEndcap, 
+            side: EndcapSide
+        ) -> Dict[ChamferPointLocation, App.Vector]:
+        chamfer_pts: Dict[ChamferPointLocation, App.Vector] = {}
+        
+        # this helps us get spline points from either the left or right side
+        # of the spline, to calculate the chamfer points
+        side_idx: int
+        match side:
+            case EndcapSide.LEFT:
+                side_idx = 0
+            case EndcapSide.RIGHT:
+                side_idx = -1
+
+        # if we have a line-type endcap, then we have an upper and lower
+        # chamfer point to calculate
+        if endcap.is_line():
+            chamfer_pts[ChamferPointLocation.UPPER] = \
+                App.Vector(self.upper_spline_pts[side_idx].x, endcap.pts[0].y, 0)
+            chamfer_pts[ChamferPointLocation.LOWER] = \
+                App.Vector(self.lower_spline_pts[side_idx].x, endcap.pts[1].y, 0)
+        # if the endcap was too small for a line, then find the midpoint
+        # between the upper and lower spline endpoints
+        else:
+            upper_spline_pt = self.upper_spline_pts[side_idx]
+            lower_spline_pt = self.lower_spline_pts[side_idx]
+            chamfer_pts[ChamferPointLocation.CENTER] = (upper_spline_pt+lower_spline_pt)/2
+        
+        return chamfer_pts     
 
     def __get_side_coords(
             self, 
             x_position: float, 
             profile: Part.Shape,
             chamfer_rad: float
-        ) -> RoundedTrapezoidSidePoints:
+        ) -> RoundedTrapezoidEndcap:
         """
             Generates end cap coordinates for one side of the rounded trapezoidal
             hole.  The end cap side should include a line and two chamfers if 
@@ -326,7 +367,7 @@ class RoundedTrapezoidControlPoints():
             b1 = (p1 + p2)/2
             line_coord_pts = [b1]
 
-        return RoundedTrapezoidSidePoints(line_coord_pts)
+        return RoundedTrapezoidEndcap(line_coord_pts)
 
     def __get_intersections(
             self, 
@@ -364,19 +405,37 @@ class RoundedTrapezoidControlPoints():
         intersections.sort(key=lambda pt: pt.y, reverse=True)
         return intersections
     
+    # TODO: when the endcap is a center point rather than a line, you really want
+    #       to keep the hole spacing uniform, you want to extend the spline out
+    #       toward the edge, and then force the rounded part to go through the
+    #       outer side point
+
     def show(self) -> None:
         self.show_items.extend(utilities.draw_points(self.upper_spline_pts))
         self.show_items.extend(utilities.draw_points(self.lower_spline_pts))
-        self.show_items.extend(utilities.draw_points(self.left_side_pts.pts))
-        self.show_items.extend(utilities.draw_points(self.right_side_pts.pts))
+        self.show_items.extend(utilities.draw_points(self.left_endcap.pts))
+        self.show_items.extend(utilities.draw_points(self.right_endcap.pts))
+        for pt_loc in ChamferPointLocation:
+            if pt_loc in self.left_chamfer_pts:
+                self.show_items.extend(utilities.draw_points([self.left_chamfer_pts[pt_loc]]))
+
+            if pt_loc in self.right_chamfer_pts:
+                self.show_items.extend(utilities.draw_points([self.right_chamfer_pts[pt_loc]]))
 
     def unshow(self) -> None:
         for doc_obj in self.show_items:
             App.ActiveDocument.removeObject(doc_obj.Name)
 
+class HoleGenerator(ABC):
+    """
+    Interface for classes that will draw lightening holes inside a wing rib
+    """
+    def generate_sketch(self, interval: HoleBoundRegion) -> Sketcher.Sketch:
+        pass
+
 class RoundedTrapezoidHoleGenerator(HoleGenerator):
     """
-    Generates one or more rounded trapezoidal hole in a single HoleBoundingRegion
+    Generates one or more rounded trapezoidal holes in a single HoleBoundingRegion
 
     Parameters
     ----------
@@ -398,11 +457,11 @@ class RoundedTrapezoidHoleGenerator(HoleGenerator):
         ) -> None:
         super().__init__()
         
-        # self.max_chamfer_rad = max_chamfer_rad
-        # self.max_hole_length = max_hole_length
-        # self.min_hole_spacing = min_hole_spacing
+        self.max_chamfer_rad = max_chamfer_rad
+        self.max_hole_length = max_hole_length
+        self.min_hole_spacing = min_hole_spacing
 
-    def generate_sketch(self, interval: HoleBoundRegion) -> Sketcher.Sketch:
+    def generate_sketch(self, bdg_region: HoleBoundRegion) -> Sketcher.Sketch:
         """
         Generate a sketch representing one or more rounded trapezoidal lightening
         holes inside the given HoleboundingRegion
@@ -413,8 +472,248 @@ class RoundedTrapezoidHoleGenerator(HoleGenerator):
             a region where the hole generator may generate one or more rib
             lightening holes
         """
-        pass
+                
+        # create the sketch on the YZ plane
+        sk: Sketcher.Sketch = App.ActiveDocument.addObject("Sketcher::SketchObject", "lightening-holes")
+        sk.Placement = utilities.xy_placement    
+
+        constraints: List[Sketcher.Constraint] = []
+
+        def draw_line(endcap: RoundedTrapezoidEndcap) -> tuple[int, Part.LineSegment]:
+            if endcap.is_line():
+                line = Part.LineSegment(endcap.pts[0], endcap.pts[1])
+                id = sk.addGeometry(line, False)
+                constraints.append(Sketcher.Constraint("Block", id))
+                return id, line
+            return -1, None
         
+        def draw_spline(pts: List[App.Vector]) -> tuple[int, Part.BSplineCurve]:
+            bsp = Part.BSplineCurve()
+            bsp.interpolate(pts)
+            id = sk.addGeometry(bsp)
+            constraints.append(Sketcher.Constraint("Block", id))
+            return id, bsp
+
+        # curve: Part.BoundedCurve
+        def get_nearest_end_to_pt(target_pt: App.Vector, curve):
+            pt_dist = [
+                (1, curve.StartPoint.distanceToPoint(target_pt)),
+                (2, curve.EndPoint.distanceToPoint(target_pt))
+            ]
+            pt_dist.sort(key=lambda x : x[1])
+            return pt_dist[0][0]
+        
+        def draw_chamfers(
+            chamfer_pts: Dict[ChamferPointLocation, App.Vector],
+            max_chamfer_rad: float,
+            line: Part.LineSegment, line_id: int,
+            upper_bsp: Part.BSplineCurve, upper_bsp_id: int,
+            lower_bsp: Part.BSplineCurve, lower_bsp_id: int,
+            endcap_side: EndcapSide
+        ) -> None:
+            """
+            Draw circular arcs that join a side boundary line with one of the bspline
+            interior boundary sections
+            """
+                
+            top_arc_start: float; top_arc_end: float
+            bottom_arc_start: float; bottom_arc_end: float
+            upper_spl_pt: App.Vector; lower_spl_pt: App.Vector
+
+            match endcap_side:
+                case EndcapSide.LEFT:
+                    top_arc_start = numpy.deg2rad(90)
+                    top_arc_end = numpy.deg2rad(180)
+                    bottom_arc_start = numpy.deg2rad(-180)
+                    bottom_arc_end = numpy.deg2rad(-90)
+                    upper_spl_pt = upper_bsp.StartPoint
+                    lower_spl_pt = lower_bsp.StartPoint
+                    
+                case EndcapSide.RIGHT:
+                    top_arc_start = numpy.deg2rad(0)
+                    top_arc_end = numpy.deg2rad(90)
+                    bottom_arc_start = numpy.deg2rad(-90)
+                    bottom_arc_end = numpy.deg2rad(0)
+                    upper_spl_pt = upper_bsp.EndPoint
+                    lower_spl_pt = lower_bsp.EndPoint
+                    
+
+            # if the space was too small to form a line with the target offset, then
+            # we create two arcs and join them directly, without a line in the middle
+            if line is None:
+                # p1 = chamfer_pts[0]
+                # p2 = chamfer_pts[1]
+                p1 = upper_spl_pt
+                p2 = lower_spl_pt
+
+                mid_pt = (p1+p2)/2
+                radius = p1.distanceToPoint(p2)/2  
+
+                arc_top = Part.ArcOfCircle(
+                    Part.Circle(
+                        mid_pt,
+                        utilities.z_axis,
+                        radius
+                    ),
+                    top_arc_start,
+                    top_arc_end
+                )
+
+                id_arc_top = sk.addGeometry(arc_top, False)
+                bsp_pt_id = 1 if upper_bsp.StartPoint == p1 else 2
+                arc_pt_id = get_nearest_end_to_pt(p1, arc_top)
+                top_arc_other_id = 1 if arc_pt_id == 2  else 2
+
+                # TODO: We really need to know why the order that we apply the 
+                #       constraints matters so much; if you do the arc first, 
+                #       everything is rejected as over-constrained 
+                constraints.append(Sketcher.Constraint("Tangent", upper_bsp_id, bsp_pt_id, id_arc_top, arc_pt_id))
+
+                arc_bottom = Part.ArcOfCircle(
+                    Part.Circle(
+                        mid_pt,
+                        utilities.z_axis,
+                        radius
+                    ),
+                    bottom_arc_start,
+                    bottom_arc_end
+                )
+
+                id_arc_bottom = sk.addGeometry(arc_bottom, False)
+                bsp_pt_id = 1 if lower_bsp.StartPoint == p2 else 2
+                arc_pt_id = get_nearest_end_to_pt(p2, arc_bottom)
+                bottom_arc_other_id = 1 if arc_pt_id == 2 else 2
+
+                constraints.append(Sketcher.Constraint("Tangent", lower_bsp_id, bsp_pt_id, id_arc_bottom, arc_pt_id))
+                constraints.append(Sketcher.Constraint("Tangent", id_arc_top, top_arc_other_id, id_arc_bottom, bottom_arc_other_id))
+            # we have a line in the middle, so create arcs that join the line on each
+            # end to the top and bottom spline sections
+            else:
+                arc_radius = max_chamfer_rad
+                top_arc_center = App.Vector(upper_spl_pt.x, line.StartPoint.y, 0.0)
+
+                arc_top = Part.ArcOfCircle(
+                    Part.Circle(
+                        top_arc_center,
+                        utilities.z_axis,
+                        arc_radius
+                    ),
+                    top_arc_start,
+                    top_arc_end
+                )
+
+                id_arc_top = sk.addGeometry(arc_top, False)
+                bsp_pt_id = 1 if upper_bsp.StartPoint == upper_spl_pt else 2
+                arc_pt_id = get_nearest_end_to_pt(upper_spl_pt, arc_top)
+                top_arc_other_id = 1 if arc_pt_id == 2  else 2
+                
+                constraints.append(Sketcher.Constraint("Tangent", upper_bsp_id, bsp_pt_id, id_arc_top, arc_pt_id))
+                constraints.append(Sketcher.Constraint("Tangent", id_arc_top, top_arc_other_id, line_id, 1))
+
+                btm_arc_ctr = App.Vector(lower_spl_pt.x, line.EndPoint.y, 0.0)
+
+                arc_bottom = Part.ArcOfCircle(
+                    Part.Circle(
+                        btm_arc_ctr,
+                        utilities.z_axis,
+                        arc_radius
+                    ),
+                    bottom_arc_start,
+                    bottom_arc_end
+                )
+
+                id_arc_bottom = sk.addGeometry(arc_bottom, False)
+                bsp_pt_id = 1 if lower_bsp.StartPoint == lower_spl_pt else 2
+                arc_pt_id = get_nearest_end_to_pt(lower_spl_pt, arc_bottom)
+                btm_arc_other_id = 1 if arc_pt_id == 2  else 2
+
+                constraints.append(Sketcher.Constraint("Tangent", lower_bsp_id, bsp_pt_id, id_arc_bottom, arc_pt_id))
+                constraints.append(Sketcher.Constraint("Tangent", id_arc_bottom, btm_arc_other_id, line_id, 2))
+
+        def get_hole_control_pts(
+                bdg_region: HoleBoundRegion, 
+                hole_spacing: float,
+                max_chamfer_rad: float,
+                max_hole_length: float
+            ) -> List[RoundedTrapezoidControlPoints]:
+            
+            # create a number of hole segments from the bounding region interval
+            # the segments do not (yet) include spacing in between holes
+            num_segments: int = math.ceil(bdg_region.interval.length()/max_hole_length)
+            if num_segments == 0:
+                return []
+            
+            # create a set of control point groups where each group is separated
+            # by at distance hole_spacing
+            ctl_pts: List[RoundedTrapezoidControlPoints] = []
+            
+            total_wall_width = (num_segments-1)*hole_spacing
+            total_hole_width = bdg_region.interval.length()-total_wall_width
+            hole_width = total_hole_width/num_segments
+
+            dist = bdg_region.interval.start
+            # for idx in range(0,num_segments-1):
+            for idx in range(0,num_segments):
+                # get the endpoints for the hole
+                start = dist
+                dist += hole_width
+                end = dist
+                
+                # get the starting point for the next hole, if there is one
+                dist += hole_spacing
+
+                # add a set of control points for the hole
+                ctl_pts.append(
+                    RoundedTrapezoidControlPoints(
+                        bdg_region.af_inner_contour, 
+                        Interval(start,end), 
+                        max_chamfer_rad
+                    )
+                )
+            return ctl_pts
+        
+        hole_ctl_pts_list = \
+            get_hole_control_pts(
+                bdg_region, 
+                self.min_hole_spacing, 
+                self.max_chamfer_rad, 
+                self.max_hole_length
+            )
+        
+        for ctl_pts in hole_ctl_pts_list:
+
+            left_line_id, left_line = draw_line(ctl_pts.left_endcap)
+            right_line_id, right_line = draw_line(ctl_pts.right_endcap)
+            upper_bsp_id, upper_bsp = draw_spline(ctl_pts.upper_spline_pts)
+            lower_bsp_id, lower_bsp = draw_spline(ctl_pts.lower_spline_pts)
+
+            draw_chamfers(
+                ctl_pts.left_chamfer_pts,
+                self.max_chamfer_rad,
+                left_line, 
+                left_line_id, 
+                upper_bsp, 
+                upper_bsp_id, 
+                lower_bsp, 
+                lower_bsp_id, 
+                EndcapSide.LEFT
+            )
+            
+            draw_chamfers(
+                ctl_pts.right_chamfer_pts, 
+                self.max_chamfer_rad,
+                right_line, 
+                right_line_id, 
+                upper_bsp, 
+                upper_bsp_id, 
+                lower_bsp, 
+                lower_bsp_id, 
+                EndcapSide.RIGHT
+            )
+
+        sk.addConstraint(constraints)
+
+        return sk
 
 class LighteningHoleBounds:
     """
@@ -646,7 +945,7 @@ def generate_hole_bounds(
             
             ref_line = Part.makeLine(top_pt, bottom_pt)
             ref_lines.append(ref_line)
-
+        # NOTE: LighteningHoleBounds actually stores control points
         bounds: List[LighteningHoleBounds] = []
         for ref_idx in range(0, num_segments):
             left = ref_lines[ref_idx].copy()
